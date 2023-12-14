daf0cbf79a6564ce5e226c97c3309fff3a55a556
avazjanovsardorbek01 <sardoravazzanov@gmail.com>
2023-12-15 01:56:19 +1000

    ice-cream

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,24 @@
 module.exports = {
-  content: ['./*.html', './pages/*.html'],
+  content: ["./*.html", "./pages/*.html"],
   theme: {
     screens: {
-      sm: '480px',
-      md: '768px',
-      lg: '976px',
-      xl: '1440px',
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
     },
     extend: {
       colors: {
-        colorFirst: '#F2CEDA',
-        colorSecond: '#D23166',
-        colorThird: '#0F0101',
-        colorFourth: '#150101',
-        colorFifth: '#fafafa',
+        colorFirst: "#F8EDF0",
+        colorSecond: "#D23166",
+        colorThird: "#0F0101",
+        colorFourth: "#150101",
+        colorFifth: "#fafafa",
       },
     },
     fontFamily: {
-      roboto: ['Roboto', 'sans-serif'],
+      roboto: ["Roboto", "sans-serif"],
+      fredoka: ["Fredoka"],
     },
   },
   plugins: [],
